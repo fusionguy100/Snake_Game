6717c2491c21f9bf676736a8dcb795e5cad0d709
fusionguy100 <jnewell1820@gmail.com>
2025-05-09 09:05:17 -0700

    updated movement, can no longer move incorrectly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+
 import time
 screen = Screen()
 screen.setup(width = 600, height = 600)
@@ -9,6 +12,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,6 +26,19 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #DETECT COLLISION WITH WALL
+    if snake.head.xcor() > 280 or  snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+
+
 
 
 
